3486ba1bfc8c528eb851097c7f4a0394c2578aa9
Karlox01 <orlandinikarlo@gmail.com>
2023-11-03 21:51:18 +0000

    lesson about using hooks to add or remove from array or list of objects

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,18 @@
 import css from './App.module.css';
+import ClassCounter from './components/ClassCounter';
+import ControlledForm from './components/ControlledForm';
+import ControlledFormHooks from './components/ControlledFormHooks';
+import HooksCounter from './components/HooksCounter';
+import UseStateWithArrays from './components/UseStateWithArrays';
 
 function App() {
   return (
     <div className={css.App}>
-      {/* Add your components here */}
+      {/* <ClassCounter />
+      <HooksCounter /> */}
+      {/* <ControlledForm /> */}
+      {/* <ControlledFormHooks /> */}
+      <UseStateWithArrays />
     </div>
   );
 }
